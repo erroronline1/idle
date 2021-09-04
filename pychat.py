0498fad080720a26c403f6d8cf585a4e0d5b022a
erroronline1 <dev@erroronline.one>
2021-09-04 22:49:01 +0200

    added a bit of user-management (current active, name taken)

--- pychat.py
+++ pychat.py
@@ -11,21 +11,23 @@ import os
 from win10toast import ToastNotifier
 
 HELLO = '''
-             _       _
- ___ _ _ ___| |_ ___| |_
-| . | | |  _|   | .'|  _|
-|  _|_  |___|_|_|__,|_|
-|_| |___|                 built 20210904
+                     _           _       _
+ ___ ___ ___ ___ ___| |_ _ _ ___| |_ ___| |_
+| .'|   | .'|  _|  _|   | | |  _|   | .'|  _|
+|__,|_|_|__,|_| |___|_|_|_  |___|_|_|__,|_|
+                        |___|                 built 20210904
 
 by error on line 1 (erroronline.one)
 '''
 
-INI={
-'database' : '\\\\192.168.178.26\\Public\\pychat.db', # e.g. on some network path
-'dblimit' : 25, # sanitize database from all entries where ID < MAX(ID) - DBLIMIT
-'update' : 2, # seconds interval to fetch contribution updates
-'user' : None, # initiate global user name, changeable
-'language' : 'en', # default language, changeable
+INI = {
+	'database' : '\\\\192.168.178.26\\Public\\anarchychat.db', # e.g. on some network path
+	'dblimit' : 25, # sanitize database from all entries where ID < MAX(ID) - DBLIMIT
+	'update' : 3, # seconds interval to fetch contribution updates, likely slightly limits read/write traffic on the drive
+	'active' : 30, # seconds to expire before a user is considered logged off. must be more than update interval
+	'user' : None, # initiate global user name, changeable
+	'language' : 'en', # default language, changeable
+	'title': 'AnarchyChat' # app title to display e.g. in notification
 }
 
 class anarchychat:
@@ -33,16 +35,18 @@ class anarchychat:
 		self.database = ini['database']
 		self.dblimit = ini['dblimit']
 		self.interval = ini['update']
+		self.active = ini['active']
 		self.user = ini['user']
 		self.language = ini['language']
+		self.title = ini['title']
 		self.notify = True
 		self.languageChunks = {
 			'setname': {
 				'en': 'enter your name, [exit] to quit: ',
 				'de': 'bitte gib deinen namen ein, [exit] um abzubrechen: '
 			}, 'greet': {
-				'en': 'hello {0}! welcome to the chat. type [help] for command overview. the chat will start with the latest {1} contributions. press enter to continue...',
-				'de': 'hallo {0}! willkommen im chat. gib [help] für eine befehlsübersicht ein. der chat startet mit den letzten {1} beiträgen. drücke enter um fortzufahren...'
+				'en': 'hello {0}! welcome to the chat. type [help] for command overview.\nthe chat will start with the latest {1} contributions. current users are {2}.\nstarting any moment...',
+				'de': 'hallo {0}! willkommen im chat. gib [help] für eine befehlsübersicht ein.\nder chat startet mit den letzten {1} beiträgen. aktuelle nutzer sind {2}.\ngleich geht es los...'
 			}, 'goodbye': {
 				'en': 'goodbye!',
 				'de': 'tschüß!'
@@ -52,6 +56,9 @@ class anarchychat:
 			}, 'left': {
 				'en': 'left the chat',
 				'de': 'hat den chat verlassen'
+			}, 'timeout': {
+				'en': ' because of timeout',
+				'de': ' weil die zeit ablief'
 			}, 'clear': {
 				'en': 'all contributions being deleted',
 				'de': 'alle beiträge gelöscht'
@@ -61,6 +68,9 @@ class anarchychat:
 			}, 'name':{
 				'en': 'enter new name: ',
 				'de': 'neuen namen eingeben: '
+			}, 'nametaken':{
+				'en': 'name already taken',
+				'de': 'name bereits belegt'
 			}, 'notify': {
 				'en': 'notifications are now {0}',
 				'de': 'benachrichtigungen sind nun {0}'
@@ -71,29 +81,36 @@ class anarchychat:
 				'en': 'turned off',
 				'de': 'ausgeschaltet'
 			}, 'help': {
-				'en': '''[exit] to quit
-[clear] to truncate database - affects all users!
+				'en': '''[clear] to truncate database - affects all users!
+[exit] to quit
 [lang] to change language
 [name] to change your name
-[notify] toggle notification on new messages''',
-				'de': '''[exit] um zu beenden
-[clear] um datenbank zu leeren - betrifft alle nutzer!
+[notify] to toggle notification on new messages
+[users] to show list of currently active users''',
+				'de': '''[clear] um datenbank zu leeren - betrifft alle nutzer!
+[exit] um zu beenden
 [lang] um die sprache zu ändern
 [name] um deinen namen zu ändern
-[notify] benachrichtigung bei neuen nachrichten an- oder ausschalten'''
+[notify] um benachrichtigung bei neuen nachrichten an- oder ausschalten
+[users] um eine liste der aktiven nutzer anzuzeigen'''
 			}
 		}
 
 		# check if table exists, otherwise create
 		self.connection = sqlite3.connect(self.database)
 		c = self.connection.cursor()
-		c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='CHAT' ''')
+		c.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='CHAT';''')
 		if not c.fetchone()[0] :
-			self.connection.execute('''CREATE TABLE CHAT
+			self.connection.executescript('''
+				CREATE TABLE CHAT
 				(ID INTEGER PRIMARY KEY AUTOINCREMENT,
 				NAME TINYTEXT NOT NULL,
 				TIME TINYTEXT NOT NULL,
-				MESSAGE TEXT NOT NULL);''')
+				MESSAGE TEXT NOT NULL);
+
+				CREATE TABLE PING
+				(NAME TINYTEXT UNIQUE,
+				TOUCH TINYTEXT);''')
 			self.connection.commit()
 		if self.login():
 			self.start()
@@ -102,10 +119,14 @@ class anarchychat:
 		# set username
 		while not self.user:
 			print(self.colorize(self.lang('setname'), Fore.GREEN))
-			self.user = str(input('> '))
+			select = str(input('> ')).strip()
+			if select in self.ping(self.connection, 'get'):
+				print(self.colorize(self.lang('nametaken'), Fore.GREEN))
+			elif len(select):
+				self.user = select
 		if self.user.lower() != '[exit]':
-			print(self.colorize(self.lang('greet', self.user, self.dblimit), Fore.GREEN))
-			input()
+			print(self.colorize(self.lang('greet', self.user, self.dblimit, ', '.join(self.ping(self.connection, 'get'))), Fore.GREEN))
+			time.sleep(3)
 			return True
 		self.exit()
 	
@@ -117,19 +138,23 @@ class anarchychat:
 
 		self.post(self.colorize(self.lang('joined'), Fore.GREEN))
 		while True:
-			message = str(input('\r> '))
-			if not self.filterinput(message):
+			try:
+				message = str(input('\r> '))
+				if not self.filterinput(message):
+					self.exit()
+			except KeyboardInterrupt:
 				self.exit()
 
 	def exit(self):
-		# exit program
+		# delete user from active table, close database connection and exit program
+		self.ping(self.connection, 'delete', {'NAME': self.user})
 		print(self.colorize(self.lang('goodbye'), Fore.GREEN))
 		self.connection.close()
 		time.sleep(2)
 		exit()
 
 	def fetch(self):
-		# fetch the latest contributions every so many seconds
+		# fetch the latest contributions every so many seconds, notify about new messages and tell about being still active 
 		# needs another connection for running in separate thread
 		fconnection = sqlite3.connect(self.database)
 		latestid = 0
@@ -146,20 +171,55 @@ class anarchychat:
 				if latestuser != self.user:
 					self.notification(latestmsg)
 				print('\r> ', end = '')
+			# ping your name to the active list
+			self.ping(fconnection, 'put')
 			time.sleep(self.interval)
 
-	def post(self, message):
-		# insert input and delete all older entries 
+	def post(self, message, user = None):
+		# insert message and delete older entries 
 		message = message.replace('\'','\'\'')
-		self.connection.execute('''INSERT INTO CHAT (ID, NAME, TIME, MESSAGE) VALUES
-			(NULL, '{0}', '{1}', '{2}');'''.format(self.user, datetime.datetime.now().strftime('%d.%m.%y %H:%M:%S'), message))
-		self.connection.execute('''DELETE FROM CHAT WHERE ID IN (SELECT ID FROM CHAT ORDER BY ID DESC LIMIT (SELECT COUNT(*) FROM CHAT) OFFSET {0});'''.format(self.dblimit))
+		self.connection.executescript('''
+			INSERT INTO CHAT (ID, NAME, TIME, MESSAGE) VALUES (NULL, '{0}', '{1}', '{2}');
+			DELETE FROM CHAT WHERE ID IN (SELECT ID FROM CHAT ORDER BY ID DESC LIMIT (SELECT COUNT(*) FROM CHAT) OFFSET {3});
+			'''.format(
+				user if user else self.user,
+				datetime.datetime.now().strftime('%d.%m.%y %H:%M:%S'),
+				message,
+				self.dblimit))
 		self.connection.commit()
 
 	def clearDB(self):
 		# truncate table
-		self.connection.execute('''DELETE FROM CHAT''')
+		self.connection.executescript('''DELETE FROM CHAT; VACUUM;''')
 		self.connection.commit()
+
+	def ping(self, conn, method, fields=None):
+		if method == 'put':
+			conn.execute('''INSERT OR REPLACE INTO PING (NAME, TOUCH) VALUES ('{0}', strftime('%s', 'now'));'''.format(self.user))
+			conn.commit()
+		elif method == 'get':
+			self.ping(conn, 'delete')
+			cursor = conn.cursor()
+			cursor.execute('''SELECT * FROM PING''')
+			results = cursor.fetchall()
+			out=[]
+			if len(results):
+				for result in results:
+					out.append(result[0])
+			return out
+		elif method == 'delete':
+			if not fields:
+				cursor = conn.cursor()
+				cursor.execute('''SELECT NAME FROM PING WHERE (strftime('%s', 'now') - TOUCH) > {0};'''.format(self.active))
+				results = cursor.fetchall()
+				if len(results):
+					for result in results:
+						if result[0] != self.user:
+							self.post(self.colorize(self.lang('left') + self.lang('timeout'), Fore.GREEN), result[0])
+				conn.execute('''DELETE FROM PING WHERE (strftime('%s', 'now') - TOUCH) > {0};'''.format(self.active))
+			else:
+				conn.execute('''DELETE FROM PING WHERE {0}='{1}';'''.format(list(fields.keys())[0], fields[list(fields.keys())[0]]))
+			conn.commit()
 
 	def colorize(self, str, col):
 		# occasionally fancy colors for outputs
@@ -178,15 +238,15 @@ class anarchychat:
 		
 	def filterinput(self, message):
 		# filter and conditionally execute commands
-		if message.lower() == '[exit]':
+		if message.lower() == '[clear]':
+			print(self.colorize(self.lang('clear'), Fore.GREEN))
+			self.clearDB()
+			return True
+		elif message.lower() == '[exit]':
 			self.post(self.colorize(self.lang('left'), Fore.GREEN))
 			return False
 		elif message.lower() == '[help]':
 			print(self.colorize(self.lang('help'), Fore.GREEN))
-			return True
-		elif message.lower() == '[clear]':
-			print(self.colorize(self.lang('clear'), Fore.GREEN))
-			self.clearDB()
 			return True
 		elif message.lower() == '[lang]':
 			supported = list(self.languageChunks['lang'].keys())
@@ -197,13 +257,19 @@ class anarchychat:
 			return True
 		elif message.lower() == '[name]':
 			print(self.colorize(self.lang('name'), Fore.GREEN))
-			select = str(input('> '))
-			if len(select.strip()):
+			select = str(input('> ')).strip()
+			if select in self.ping(self.connection, 'get'):
+				print(self.colorize(self.lang('nametaken'), Fore.GREEN))
+			elif len(select):
+				self.ping(self.connection, 'delete', {'NAME': self.user})
 				self.user = select
 			return True
 		elif message.lower() == '[notify]':
 			self.notify = not self.notify
-			print (self.colorize(self.lang('notify', self.lang('on') if self.notify else self.lang('off')), Fore.GREEN))
+			print(self.colorize(self.lang('notify', self.lang('on') if self.notify else self.lang('off')), Fore.GREEN))
+			return True
+		elif message.lower() == '[users]':
+			print(self.colorize(','.join(self.ping(self.connection, 'get')), Fore.GREEN))
 			return True
 		else:
 			terminalwidth, terminalheight = shutil.get_terminal_size(0)
@@ -211,7 +277,7 @@ class anarchychat:
 			# may be ugly if console dimensions are changes during use
 			for i in range(0, math.ceil(len(message) / terminalwidth)):
 				terminalheight = terminalheight + i # sometimes linter can be annoying...
-				print('\033[A', ' ' * (terminalwidth - 2), '\033[A')
+				print('\033[A' + (' ' * (terminalwidth - 2)) + '\033[A')
 			if len(message.strip()):
 				self.post(message)
 			return True
@@ -222,9 +288,8 @@ class anarchychat:
 			if not hasattr(self, 'toast'):
 				self.toast = ToastNotifier()
 			# winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
-			self.toast.show_toast(msg['title'], msg['msg'], threaded=True,
-                   icon_path=None, duration=3)
+			self.toast.show_toast(self.title + ' | ' + msg['title'], msg['msg'], threaded = True, icon_path = None, duration = 3)
 
 if __name__ == '__main__':
 	print(HELLO)
-	chat=anarchychat(INI)
+	chat = anarchychat(INI)
