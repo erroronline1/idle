1882a67938cc4d52b53a8b30100f766dc9ae74c4
erroronline1 <dev@erroronline.one>
2021-09-11 23:30:26 +0200

    refactored language model for generic responses for easier readability

--- stupidbot.py
+++ stupidbot.py
@@ -3,6 +3,8 @@ import re
 
 class stupidbot():
 	def __init__(self, name):
+		self.expects = None
+		self.name = name
 		self.default = {
 			'en': [
 				'{0}, i don\'t know how to answer that...', 
@@ -11,53 +13,59 @@ class stupidbot():
 				'{0}, ich weiß nicht wie ich darauf antworten soll...',
 				'da bin ich mir leider nicht sicher {0}.']
 		}
-		self.generic = { # index 0: trigger pattern, any other: random response
-			'en': [
-				['hello',
+		self.generic = [ # first item: mulilanguage trigger pattern, second item: language dict with random responses
+			['hello|hallo', {
+				'en': [
 					'hello {0}!',
 					'{0} hi!',
 					'{0} wassuuuppp?'],
-				['bored|boring',
+				'de': [
+					'hallo {0}!',
+					'{0} hi!',
+					'{0} was geht?']}],
+			['bored|boring|langweilig|langeweile', {
+				'en': [
 					'{0} you could tidy up your room...',
 					'{0} have you finished your homework?',
-					'you have what it takes to exercise coding {0}!'],
-				['stupid|dumb',
+					'you have what it takes to exercise coding {0}!',
+					'ask me about training mental arithmetic {0}!'],
+				'de': [
+					'{0} du könntest dein zimmer aufräumen...',
+					'{0} hast du deine hausaufgaben schon fertig?',
+					'du hast alles was du zum programmieren üben brauchst {0}!',
+					'du, {0}, und ich könnten kopfrechnen üben!']}],
+			['stupid|dumb|blöd|dumm', {
+				'en': [
 					'{0} are you sure about that?',
 					'that\'s not nice to say about anyone {0}!'],
-				['joke',
+				'de': [
+					'{0} bist du dir da sicher?',
+					'das ist aber nicht nett {0}!']}],
+			['joke|witz', {
+				'en': [
 					'Q. Are any Halloween monsters good at math? A. No — unless you Count Dracula!',
 					'Q. Why is Peter Pan flying all the time? A. He Neverlands!',
 					'Q: What is a skeleton\'s favorite musical instrument? A: A Trombone!',
 					'Q: How do you stay warm in an empty room? A: Go stand in the corner — it\'s always 90 degrees.'],
-				['funny|haha',
-					'i try my very best.']
-			],
-			'de': [
-				['hallo',
-					'hallo {0}!',
-					'{0} hi!',
-					'{0} was geht?'],
-				['langweilig|langeweile',
-					'{0} du könntest dein zimmer aufräumen...',
-					'{0} hast du deine hausaufgaben schon fertig?',
-					'du hast alles was du zum programmieren üben brauchst {0}!'],
-				['blöd|dumm',
-					'{0} bist du dir da sicher?',
-					'das ist aber nicht nett {0}!'],
-				['witz',
+				'de': [
 					'Treffen sich 2 Schnecken an der Straße. Will die eine herübergehen. Sagt die andere: "Vorsichtig in einer Stunde kommt der Bus."',
 					'Ein Mann rennt völlig außer Atem zum Bootssteg, wirft seinen Koffer auf das drei Meter entfernte Boot, springt hinterher, zieht sich mit letzter Kraft über die Reling und schnauft erleichtert: "Geschafft!" Einer der Seeleute: "Gar nicht so schlecht, aber warum haben Sie eigentlich nicht gewartet, bis wir anlegen?"',
 					'Paul zerscheppert in der Wohnung seines Onkels eine große Vase. Der erblasste Onkel stammelt: "Die Vase war aus dem 17. Jahrhundert!" Darauf Paul erleichtert: "Gott sei Dank, ich dachte schon, sie sei neu".',
 					'Laufen zwei Zahnstocher den Berg hoch und werden plötzlich von einem Igel überholt. Sagt der eine zum anderen: "Ach – hätte ich gewusst, dass ein Bus fährt, wäre ich mit dem gefahren!"',
 					'Fritzchen sitzt am See und angelt. Ein Spaziergänger fragt: "Und, beißen die Fische?" Fritzchen antwortet entnervt: "Nein, Sie können sie, ruhig streicheln."',
 					'Junge: "Was ist ein Rotkehlchen?" Schwester: "Ach, irgend so ein verrückter Fisch!" Junge: "Hier steht aber: Hüpft von Ast zu Ast!" Schwester: "Da siehst du, wie verrückt der ist!"',
-					'Fragt der Lehrer: "Wer von euch kann mir sechs Tiere nennen, die in Australien leben?" Meldet sich Fritzchen: "Ein Koala und fünf Kängurus.'],
-				['lustig|haha',
-					'ich versuche mein bestes!']
-			]
-		}
-		self.expects = None
-		self.name = name
+					'Fragt der Lehrer: "Wer von euch kann mir sechs Tiere nennen, die in Australien leben?" Meldet sich Fritzchen: "Ein Koala und fünf Kängurus.']}],
+			['funny|lustig|haha', {
+				'en': [
+					'i try my very best.'],
+				'de': [
+					'ich versuche mein bestes!']}],
+			['skill|can you|fähigkeit|kannst du', {
+				'en': [
+					'i\'m glad you asked {0}! currently i can recommend activities in case you are bored, tell a few stupid jokes and help you train mental arithmetics. interact with me by mentioning me with @' + self.name + '.'],
+				'de': [
+					'schön dass du fragst {0}! derzeit kann ich dir was gegen langeweile empfehlen, ein paar dumme witze erzählen und dir beim kopfrechnen üben helfen. sprich mit mir indem du mich mit @' + self.name + 'erwähnst.']}]
+		]
 
 	def parse(self, message, language, user):
 		# update with every call for being able to be changed during runtime of chat even after init of class
@@ -66,22 +74,29 @@ class stupidbot():
 		self.user = user
 		if '@'+self.name in message:
 			answer = False
-			# list of skills according to method names. these have to accept message as parameter
-			# and have to provide their own trigger patterns (e.g. concatenated in all supported languages)
+			# list of skills according to method names.
 			skills = ['mentalarithmetic']
 			for skill in skills:
 				answer = getattr(self, skill)(message)
 			# if skills did non respond search generic responses
 			if not answer:
-				for keywords in self.generic[language]:
+				for keywords in self.generic:
 					if re.search(keywords[0], message, re.IGNORECASE | re.DOTALL):
-						answer = keywords[random.randint(1, len(keywords) - 1)].format('@' + user)
+						answer = keywords[1][language][random.randint(0, len(keywords[1][language]) - 1)].format('@' + user)
 						break
 			# default answer if still empty
 			if not answer:
 				answer = self.default[language][random.randint(0, len(self.default[language]) - 1)].format('@' + user)
 			return answer
 		return False
+
+	################################################################
+	# skill methods
+	# 
+	# these have to accept message as parameter
+	# and have to provide their own trigger patterns (e.g. concatenated in all supported languages)
+	# occasionally update the help text and recommendations on being bored while you're hardcoding here anyway
+	################################################################
 
 	def mentalarithmetic(self, message):
 		if (re.search('calculate|math|mental|rechnen', message, re.IGNORECASE | re.DOTALL) or
